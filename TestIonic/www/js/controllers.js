3806bc2aeeeb88cd140a2d5f1bd4ecf1e46a80b1
Tasty <colin.cros@gmail.com>
2017-01-11 17:59:57 +0100

    Recherche annuaire

--- TestIonic/www/js/controllers.js
+++ TestIonic/www/js/controllers.js
@@ -1,6 +1,12 @@
 angular.module('starter.controllers', [])
 /*Cooolin*/
 
+.filter('customFilter', function(){
+    return function(x){
+      return {'id':1, 'nom':"Cros", 'prenom':"Colin", 'numero':"06 79 22 11 88", 'role':"Patron"};
+    };
+})
+
 .controller('AnnuaireCtrl',function($scope,$http){
   //monnaies pour la requete "USD","EUR","CNY","HUF","CAD"
   //https://www.omdbapi.com/ pour une autre base de donnée
@@ -8,10 +14,13 @@ angular.module('starter.controllers', [])
       .success(function(response){
         //$scope.texte = response.query.results.rate[0].Rate;
         //$scope.rate = response.query.results.rate[0]
-        $scope.message = response[0]
+       $scope.message = response[0]
         
       });
-
+  $scope.listeDeContacts = [
+      {'id':1, 'nom':"Cros", 'prenom':"Colin", 'numero':"06 79 22 11 88", 'role':"Patron"},
+      {'id':2, 'nom':"Sobkowicz", 'prenom':"Konrad", 'numero':"06 07 09 11 52", 'role':"SG"},
+      {'id':2, 'nom':"Haguenaueur", 'prenom':"Timothée", 'numero':"06 22 36 58 14", 'role':"Suceur"}];
 })
 // convertit un objet en chaine JSON 
 //JSON.stringify()
