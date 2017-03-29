c5fa1243eb0f0221beb956a880ddb5831288bb06
Dark-Mahr <pierre.de-villenaut@ecl16.ec-lyon.fr>
2017-03-29 15:11:57 +0200

    Onglet info terminé (?)

--- TestIonic/www/js/app.js
+++ TestIonic/www/js/app.js
@@ -118,7 +118,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'FilDActualiteCtrl'
       }
     }
-  });
+  })
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/annuaire');
